7488b72f740574ad5128b12929600fcf1dbcd063
victor-welter <victorwelter2003@gmail.com>
2023-10-19 16:10:58 -0300

    Closes #1 - Criado algoritmo para calcular regra de três

---
+++ regra_tres.py
@@ -0,0 +1,10 @@
+# Solicitar ao usuário que insira os valores
+A = float(input("Digite o valor de A: "))
+B = float(input("Digite o valor de B: "))
+C = float(input("Digite o valor de C: "))
+
+# Calcula o valor X
+X = (C * B) / A
+
+# Exibe o resultado
+print(f"O resultado é: {X}")
